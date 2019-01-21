6624fb4c5025268900f7373e7a660cea1955203e
kxgv <guinakevin@gmail.com>
2019-01-21 20:56:56 +0100

    Added MediaPlayer, SoundPool and sound1. Added Raw, inside, sound1. Added selector.xml, inside drawable.

--- app/src/main/java/org/escoladeltreball/dam5184295/pt21/MainActivity.java
+++ app/src/main/java/org/escoladeltreball/dam5184295/pt21/MainActivity.java
@@ -1,30 +1,50 @@
 package org.escoladeltreball.dam5184295.pt21;
 
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.media.AudioManager;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener, View.OnClickListener{
 
+    MediaPlayer mediaPlayer;
     ImageButton button1, button2, button3, button4;
+    private int SoundID;
+    boolean loaded;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         ImageButton button1 = findViewById(R.id.button1);
         ImageButton button2 = findViewById(R.id.button2);
         ImageButton button3 = findViewById(R.id.button3);
         ImageButton button4 = findViewById(R.id.button4);
-        
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        SoundPool soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        SoundID = soundPool.load(this, R.raw.sound1, 1);
+
+        //TODO -- agregar setOnClickListener al button1, agregar OnLoadCompleteListener
+        //button1.setOnClickListener();
+
+        // TODO -- descargar los sonidos de --> http://rpg.hamsterrepublic.com/ohrrpgce/Free_Sound_Effects
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -66,6 +86,12 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+
+        if (loaded == true){
+            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            SoundPool soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+            Log.d("Test", "Playing songs");
+        }
         return false;
     }
 }
